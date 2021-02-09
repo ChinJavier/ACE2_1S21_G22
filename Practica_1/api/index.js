aafafc759cf1e500aa81ea671c6f3173c60d9dfd
PabloAndresArg <PabloAndresArg@gmail.com>
2021-02-08 19:45:08 -0600

    iniciando conexion con mongodb , por el momento esta local
    -.-

--- Practica_1/api/index.js
+++ Practica_1/api/index.js
@@ -1,11 +1,23 @@
+const cors = require('cors');
 const express = require('express');
+
 require('dotenv').config();
+require('./database');
+
 const app = express();
 const { router } = require('./routes/routes');
 
-app.use(express.json());
-app.use('/api/v1/auth', router);
+// settings
+app.set('port',  process.env.PORT || 3000);
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`);
+// middlawares
+app.use(cors());// PARA LA COMUNICACION CON ANGULAR
+app.use(express.json());
+
+
+app.use('/api/v1/auth', router);
+app.use('/api/v1/auth', (req,res) => {res.send( {text : `servidor en el puerto ${app.get('port')}` })});
+
+app.listen(app.get('port') , () => {
+    console.log(`Server running on port ${app.get('port')}`);
 });
