a05b6bc2261ad731c633362bfc971163334dd540
PabloAndresArg <PabloAndresArg@gmail.com>
2021-02-14 03:23:06 -0600

    interfaz de oxigeno

---
+++ Practica_1/WebApp/src/app/components/ui-oxygen/ui-oxygen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ui-oxygen',
+  templateUrl: './ui-oxygen.component.html',
+  styleUrls: ['./ui-oxygen.component.css']
+})
+export class UiOxygenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
