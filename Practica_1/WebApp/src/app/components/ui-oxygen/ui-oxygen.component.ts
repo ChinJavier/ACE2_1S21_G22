ddac98eaa214f61a4692a79fdab23975ac7f376f
PabloAndresArg <PabloAndresArg@gmail.com>
2021-02-14 20:21:39 -0600

    modificaciones y interfaz de temperatura
    ok

--- Practica_1/WebApp/src/app/components/ui-oxygen/ui-oxygen.component.ts
+++ Practica_1/WebApp/src/app/components/ui-oxygen/ui-oxygen.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router'; // PARA CAMBIAR DE UNA PESTAÑA A OTRA
 @Component({
   selector: 'app-ui-oxygen',
   templateUrl: './ui-oxygen.component.html',
   styleUrls: ['./ui-oxygen.component.css']
 })
 export class UiOxygenComponent implements OnInit {
-
-  constructor() { }
+  value: any
+  constructor(private r:Router) {
+    this.value = 0;
+  }
 
   ngOnInit(): void {
   }
