bf6ccc1e2fa6f756f52629a7ad4bc9358328ce79
PabloAndresArg <PabloAndresArg@gmail.com>
2021-02-15 20:18:37 -0600

    ya estan las rutas para appIventor

--- Practica_1/api/routes/routesMux.js
+++ Practica_1/api/routes/routesMux.js
@@ -1,11 +1,53 @@
 const { Router } = require('express');
 const routerTest = Router();
-routerTest.route('/sensores/').post(
+
+var OXYGEN = 0 , TEMPERATURE = 0 , RHYTHM= 0;
+
+// routerTest.route('/sensores/:valor').post(
+//     async(req,res) => {
+//     console.log("post 1 parametros" , '->' , req.params);
+//     res.send({text:'1 parametros enviado'});
+// });
+
+// routerTest.route('/sensores/:valor/:valor2').post(
+//     async(req,res) => {
+//     console.log("POST 2 parametros" , '->' , req.params);
+//     res.send({text:'PETICION DE TEST DE SENSORES REALIZADA'});
+// });
+
+
+// SUPONGO QUE ESTE USAREMOS
+routerTest.route('/sensores/:oxigeno/:temperatura/:ritmo').post(
     async(req,res) => {
-    console.log(req.body);
-    console.log("PETICION DE TEST DE SENSORES REALIZADA")
+    const {ritmo , temperatura , oxigeno} = req.params;
+    RHYTHM = ritmo;
+    TEMPERATURE = temperatura;
+    OXYGEN = oxigeno;
+    console.log("POST 3 parametros" , '->' , req.params);
     res.send({text:'PETICION DE TEST DE SENSORES REALIZADA'});
 });
+
+// para probar la conexion
+routerTest.route('/sensores/').get(
+    async(req,res) => {
+        res.send({text:'EL SERVER PARA LOS SENSORES ESTA A LA ESCUCHA'});
+});
+
+
+// DEVOLUCION DE DATOS
+routerTest.route('/puntual/oxygen').get(
+    async(req, res) => { res.send(OXYGEN);}
+);
+
+routerTest.route('/puntual/temperature').get(
+    async(req, res) => { res.send(TEMPERATURE);}
+);
+
+routerTest.route('/puntual/rhythm').get(
+    async(req, res) => { res.send(RHYTHM);}
+);
+
+
 module.exports = {
     routerTest,
 };
