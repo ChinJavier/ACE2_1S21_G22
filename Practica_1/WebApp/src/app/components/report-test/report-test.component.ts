394a9144785f15c5895d8f551601b8919c59a974
Lourdes Lorenzana <lourdeslorenochoa@gmail.com>
2021-03-17 22:01:31 -0600

    getMediciones

--- Practica_1/WebApp/src/app/components/report-test/report-test.component.ts
+++ Practica_1/WebApp/src/app/components/report-test/report-test.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MedicionesService } from 'src/app/services/mediciones.service';
 
 @Component({
   selector: 'app-report-test',
@@ -6,9 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./report-test.component.css']
 })
 export class ReportTestComponent implements OnInit {
+  medicionesOxigeno=[];
+  medicionesTemperatura=[];
+  medicionesRitmo=[];
+  constructor(private medicionesService: MedicionesService) { 
+    this.getMedicionesOxygen();
+    this.getMedicionesRitmoC();
+    this.getMedicionesTemperatura();
+  }
 
-  constructor() { }
-
+  getMedicionesOxygen(){
+    let user=localStorage.getItem('uid');
+    this.medicionesService.getMediciones('oxygen',user).subscribe(res=>{
+      this.medicionesOxigeno =res;
+      console.log(this.medicionesOxigeno);
+    })
+  }
+  getMedicionesTemperatura(){
+    let user=localStorage.getItem('uid');
+    this.medicionesService.getMediciones('temperature',user).subscribe(res=>{
+      this.medicionesTemperatura =res;
+    })
+  }
+  getMedicionesRitmoC(){
+    let user=localStorage.getItem('uid');
+    this.medicionesService.getMediciones('rythm',user).subscribe(res=>{
+      this.getMedicionesRitmoC =res;
+    })
+  }
+   
+  
   ngOnInit(): void {
   }
 
