5790a39897613d1edb6d50af1644c1705508f386
carlosngv <carlosngv99@hotmail.com>
2021-03-20 14:35:17 -0600

    hola

--- Practica_1/WebApp/src/app/shared/URL.ts
+++ Practica_1/WebApp/src/app/shared/URL.ts
@@ -1,2 +1,4 @@
-export const baseURL = 'https://tve-app-g22.herokuapp.com/api/v1/';
-export const baseURL_sensores = 'https://tve-app-g22.herokuapp.com/sensores/';
+/* export const baseURL = 'https://tve-app-g22.herokuapp.com/api/v1/';
+export const baseURL_sensores = 'https://tve-app-g22.herokuapp.com/sensores/'; */
+export const baseURL = 'http://localhost:3000/api/v1/';
+export const baseURL_sensores = 'http://localhost:3000/sensores/';
